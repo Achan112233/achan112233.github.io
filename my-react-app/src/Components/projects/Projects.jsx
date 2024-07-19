a1123c98b4d48bfa0ce5eca9e98a227880ea4710
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2024-07-19 00:36:54 -0400

    Changed up formatting and colors

--- my-react-app/src/Components/projects/Projects.jsx
+++ my-react-app/src/Components/projects/Projects.jsx
@@ -2,9 +2,9 @@ import './projects.css'
 
 function Projects(){
     const projects = [
-        { name: 'Hungry Snake', description: 'Built a full-stack e-commerce site using React, Node.js, and MongoDB.' },
-        { name: 'HTML Website Generator', description: 'Created a weather application using React and integrating with a third-party API.' },
-        { name: 'Task Management Tool', description: 'Developed a Kanban-style task management app with React and Redux.' },
+        { name: 'Hungry Snake', description: 'Worked within a team to develop a fully functioning game that mimics the classic arcade game Snake and added different innovative game modes to make the game more stimulating.' },
+        { name: 'HTML Website Generator', description: 'Created a solution generating a comprehensive HTML shell for users, providing a solid foundation to build upon and tailor to their specific needs' },
+        { name: 'Cubey Goes To School', description: 'Developed a WebGame that simulates a day to day life at school in a fun and challenging way using processing' },
       ];
 
       return(
