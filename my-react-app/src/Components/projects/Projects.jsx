b20edadd1b9851f250ea1f2274a62f2e9b22bb1d
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2024-08-05 00:41:44 -0400

    Updated project description

--- my-react-app/src/Components/projects/Projects.jsx
+++ my-react-app/src/Components/projects/Projects.jsx
@@ -3,8 +3,8 @@ import './projects.css'
 function Projects(){
     const projects = [
         { name: 'Hungry Snake', description: 'Worked within a team to develop a fully functioning game that mimics the classic arcade game Snake and added different innovative game modes to make the game more stimulating.' },
-        { name: 'HTML Website Generator', description: 'Created a solution generating a comprehensive HTML shell for users, providing a solid foundation to build upon and tailor to their specific needs' },
-        { name: 'Cubey Goes To School', description: 'Developed a WebGame that simulates a day to day life at school in a fun and challenging way using processing' },
+        { name: 'HTML Website Generator', description: 'Created a solution generating a comprehensive HTML shell for users, providing a solid foundation to build upon and tailor to their specific needs.' },
+        { name: 'Cubey Goes To School', description: 'Developed a WebGame that simulates a day to day life at school in a fun and challenging way using processing.' },
       ];
 
       return(
